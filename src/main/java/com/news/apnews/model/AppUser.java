dee42e780c46a8e7caf457cd72f3e98ff80d7b2d
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2026-06-24 22:21:08 +0530

    Update

--- src/main/java/com/news/apnews/model/AppUser.java
+++ src/main/java/com/news/apnews/model/AppUser.java
@@ -1,15 +1,9 @@
 package com.news.apnews.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "app_users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AppUser {
 
     @Id
@@ -26,4 +20,46 @@ public class AppUser {
     private String role;
 
     private boolean enabled = true;
+
+    public AppUser() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
